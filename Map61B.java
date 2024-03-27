f512cea9bb320a4133cd44d890df59854352aff6
JLin-25 <151233145+JLin-25@users.noreply.github.com>
2024-03-26 23:33:53 -0700

    Update Map61B.java

--- Map61B.java
+++ Map61B.java
@@ -1,49 +1,196 @@
 package hashmap;
 
-import java.util.Set;
+import java.util.*;
+
 /**
- * Your implementation hashmap.MyHashMap should implement this interface. To do so,
- * append "implements hashmap.Map61B<K, V>" to the end of your "public class..."
- * declaration, though you can use other formal type parameters if you'd like.
+ *  A hash table-backed Map implementation. Provides amortized constant time
+ *  access to elements via get(), remove(), and put() in the best case.
+ *
+ *  Assumes null keys will never be inserted, and does not resize down upon remove().
+ *  @author YOUR NAME HERE
  */
-public interface Map61B<K, V> extends Iterable<K> {
-    /** Removes all of the mappings from this map. */
-    void clear();
+public class MyHashMap<K, V> implements Map61B<K, V> {
+    
+    private Collection<Node>[] buckets;
+    // You should probably define some more!
+    private int n,m;
+    private double load=0.75;
 
-    /** Returns true if this map contains a mapping for the specified key. */
-    boolean containsKey(K key);
+    /** Constructors */
+    public MyHashMap() {
+        m=16;
+        buckets=createTable(m);
+    }
+
+    public MyHashMap(int initialSize) {
+        m=initialSize;
+        buckets=createTable(m);
+    }
+    /**
+     * MyHashMap constructor that creates a backing array of initialSize.
+     * The load factor (# items / # buckets) should always be <= loadFactor
+     *
+     * @param initialSize initial size of backing array
+     * @param maxLoad maximum load factor
+     */
+    public MyHashMap(int initialSize, double maxLoad) {
+        m=initialSize;
+        buckets=createTable(m);
+        load=maxLoad;
+    }
+    @Override
+    public void clear() {
+        buckets=createTable(m);
+        n=0;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        if(buckets[mod(key)]==null)return false;
+        for (Node node:buckets[mod(key)]) {
+            if (node.key.equals(key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public V get(K key) {
+        if(buckets[mod(key)]==null)return null;
+        for (Node node:buckets[mod(key)]) {
+            if (node.key.equals(key)) {
+                return node.value;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return n;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        if(get(key)!=null)remove(key);
+        n++;
+        if(buckets[mod(key)]==null)buckets[mod(key)]=new LinkedList<Node>();
+        buckets[mod(key)].add(createNode(key,value));
+        if((double)n/m>load)resize();
+    }
+    private void resize() {
+        ArrayList<Node> arr = new ArrayList<>();
+        for (Collection<Node> bucket : buckets) {
+            if (bucket == null) continue;
+            for (Node node:bucket) {
+                arr.add(node);
+            }
+        }
+        m*=2;
+        buckets=createTable(m);
+        n=0;
+        for (Node node:arr) {
+            put(node.key,node.value);
+        }
+    }
+
+    @Override
+    public Set<K> keySet() {
+        Set<K>s=new HashSet<>();
+        for(Collection<Node> bucket:buckets){
+            if(bucket==null)continue;
+            for(Node node:bucket){
+                s.add(node.key);
+            }
+        }
+        return s;
+    }
+
+    @Override
+    public V remove(K key) {
+        if(get(key)==null)return null;
+        n--;
+        for(Node node:buckets[mod(key)]){
+            if(node.key.equals(key)){
+                buckets[mod(key)].remove(node);
+                return node.value;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+    private int mod(K n){
+        return Math.floorMod(n.hashCode(),m);
+    }
+    /**
+     * Protected helper class to store key/value pairs
+     * The protected qualifier allows subclass access
+     */
+    protected class Node {
+        K key;
+        V value;
+
+        Node(K k, V v) {
+            key = k;
+            value = v;
+        }
+    }
+
+
+
+
 
     /**
-     * Returns the value to which the specified key is mapped, or null if this
-     * map contains no mapping for the key.
+     * Returns a new node to be placed in a hash table bucket
      */
-    V get(K key);
-
-    /** Returns the number of key-value mappings in this map. */
-    int size();
+    private Node createNode(K key, V value) {
+        return new Node(key,value);
+    }
 
     /**
-     * Associates the specified value with the specified key in this map.
-     * If the map previously contained a mapping for the key,
-     * the old value is replaced.
+     * Returns a data structure to be a hash table bucket
+     *
+     * The only requirements of a hash table bucket are that we can:
+     *  1. Insert items (`add` method)
+     *  2. Remove items (`remove` method)
+     *  3. Iterate through items (`iterator` method)
+     *
+     * Each of these methods is supported by java.util.Collection,
+     * Most data structures in Java inherit from Collection, so we
+     * can use almost any data structure as our buckets.
+     *
+     * Override this method to use different data structures as
+     * the underlying bucket type
+     *
+     * BE SURE TO CALL THIS FACTORY METHOD INSTEAD OF CREATING YOUR
+     * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
      */
-    void put(K key, V value);
-
-    /** Returns a Set view of the keys contained in this map. */
-    Set<K> keySet();
+    protected Collection<Node> createBucket() {
+        return new LinkedList<>();
+    }
 
     /**
-     * Removes the mapping for the specified key from this map if present.
-     * Not required for Lab 8. If you don't implement this, throw an
-     * UnsupportedOperationException.
+     * Returns a table to back our hash table. As per the comment
+     * above, this table can be an array of Collection objects
+     *
+     * BE SURE TO CALL THIS FACTORY METHOD WHEN CREATING A TABLE SO
+     * THAT ALL BUCKET TYPES ARE OF JAVA.UTIL.COLLECTION
+     *
+     * @param tableSize the size of the table to create
      */
-    V remove(K key);
+    private Collection<Node>[] createTable(int tableSize) {
+        return new Collection[tableSize];
+    }
 
-    /**
-     * Removes the entry for the specified key only if it is currently mapped to
-     * the specified value. Not required for Lab 8. If you don't implement this,
-     * throw an UnsupportedOperationException.
-     */
-    V remove(K key, V value);
+
 }
-
